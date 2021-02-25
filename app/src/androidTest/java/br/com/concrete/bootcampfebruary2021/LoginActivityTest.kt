5efaf1887b9cc13f4bf6d9db3fc1ab73c42c0be3
Wesley Jonathan Marcolino <w.jonathan.marcolino@accenture.com>
2021-02-25 10:42:41 -0300

    Add LoginActivity layout

---
+++ app/src/androidTest/java/br/com/concrete/bootcampfebruary2021/LoginActivityTest.kt
@@ -0,0 +1,9 @@
+package br.com.concrete.bootcampfebruary2021
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginActivityTest {
+
+}
